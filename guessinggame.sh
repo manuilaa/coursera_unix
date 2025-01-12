86b5a4feea4a184fa6da6a42bbbf2952dad89ed5
manuilaa <manal.lembahej@gmail.com>
2025-01-12 14:05:02 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,44 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-function count_files() {
-    local count=$(ls -1A | wc -l)
-    echo $count
+# Function to get the number of files in the current directory
+function get_file_count {
+  echo $(ls -1 | wc -l)
 }
 
-function validate_input() {
-    if ! [[ $1 =~ ^[0-9]+$ ]]; then
-        echo 0
-        return
-    fi
-    echo 1
-}
-
-# Get the actual number of files
-file_count=$(count_files)
-
-# Welcome message
+# Main game loop
 echo "Welcome to the Guessing Game!"
-echo "Try to guess how many files are in the current directory."
-echo "----------------------------------------"
-
+file_count=$(get_file_count)
 while true; do
-    # Prompt for guess
-    read -p "Enter your guess: " user_guess
-    
-    # Validate input
-    if [ $(validate_input $user_guess) -eq 0 ]; then
-        echo "Please enter a valid positive number"
-        continue
-    fi
+  echo "Guess how many files are in the current directory:"
+  read user_guess
+  
+  if [[ ! $user_guess =~ ^[0-9]+$ ]]; then
+    echo "Please enter a valid number."
+    continue
+  fi
 
-    # Compare guess with actual count
-    if [ $user_guess -lt $file_count ]; then
-        echo "Too low! Try again."
-    elif [ $user_guess -gt $file_count ]; then
-        echo "Too high! Try again."
-    else
-        echo "Congratulations! That's the correct answer!"
-        echo "There are $file_count files in the current directory."
-        break
-    fi
+  if [[ $user_guess -lt $file_count ]]; then
+    echo "Your guess is too low. Try again."
+  elif [[ $user_guess -gt $file_count ]]; then
+    echo "Your guess is too high. Try again."
+  else
+    echo "Congratulations! You guessed it correctly."
+    break
+  fi
 done
